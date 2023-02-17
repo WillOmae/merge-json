6c597297b61ffa5840d0d06cd9e4c55a77eb6d53
Wilbur Omae <wilburomae@gmail.com>
2023-02-17 14:28:44 +0300

    Sort keys and display output

--- merge-json.js
+++ merge-json.js
@@ -1,22 +1,21 @@
 function merge() {
-    let dataInput = document.querySelector("#datasource");
-    let data = dataInput.value;
-    let lines = data.split("\n");
-    let arr = lines.map(line => JSON.parse(line));
-    const base = arr.shift();
-    console.log("BASE: " + Object.keys(base).length);
-    console.log(base);
-    const final = {};
-    Object.keys(base).forEach(k => {
-        final[k] = base[k];
-    });
-    console.log("OTHERS");
-    arr.forEach(obj => {
-        console.log(obj);
+    // DOM elements
+    const dataInput = document.getElementById("datasource");
+    const dataOutput = document.getElementById("output");
+
+    // merge JSON strings
+    const base = {};
+    dataInput.value.split("\n").map(line => JSON.parse(line)).forEach(obj => {
         Object.keys(obj).forEach(k => {
-            final[k] = obj[k];
+            base[k] = obj[k];
         })
     });
-    console.log("FINAL: " + Object.keys(final).length);
-    console.log(final);
+
+    // sort the keys
+    const final = Object.keys(base).sort().reduce((acc, key) => {
+        acc[key] = base[key];
+        return acc;
+    }, {});
+
+    dataOutput.innerText = JSON.stringify(final);
 }
